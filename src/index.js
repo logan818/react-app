7ad25bd3b363aebdc0853a9c696e1287481f65c4
logan818 <logan.calhoun45@gmail.com>
2020-08-10 21:40:39 -0400

    nothing much

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import nodemon from 'nodemon'
+
+function getButtonText() {
+  return 'Click me!!'
+}
 
 // * Create a react component
 
 const App = () => {
   return (
     <div>
-      <label class="label" for="name">
+      <label className="label" for="name">
         Enter Name:
       </label>
       <input id="name" type="text" />
-      <button style="background-color: blue; color: white;">Submit</button>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()}
+      </button>
     </div>
   )
 }
